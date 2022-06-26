ded88615e3dd3d4209b671b68fef1a05fb81b6f6
Andrea <82764646+Dozzy85@users.noreply.github.com>
2022-06-26 22:03:14 +0200

    Update Lets Create some lists.py

--- Python Fundamentals/Lets Create some lists.py
+++ Python Fundamentals/Lets Create some lists.py
@@ -27,3 +27,26 @@ print(w)
 
 w2 = list(range(100, 201, 10)) #[100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
 print(w2)
+print(len(w2))
+
+#      0    1    2    3    4
+#     -5   -4   -3   -2   -1
+w3 = ["a", "b", "c", "d", "e"]
+print(w3)
+print(w3[-1])
+print(w3[-3])
+
+#Slicing
+#           0    1    2    3    4    5    6    7    8    9
+#          -10  -9   -8   -7   -6   -5   -4   -3   -2   -1
+my_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "J"]
+print(my_list[:]) # Tutta la lista ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'J']
+print(my_list[:7]) # Dall'inizio fino all'elemento in posiozione 7 escluso ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(my_list[2:]) # Dalla posizione 2 compreso fino alla fine ['c', 'd', 'e', 'f', 'g', 'h', 'i', 'J']
+#Equivalente
+print(my_list[-8:-3]) # ['c', 'd', 'e', 'f', 'g']
+print(my_list[2:7]) # ['c', 'd', 'e', 'f', 'g']
+print(my_list[-8:7])
+#Advance Slicing
+print(my_list[2:9:2]) # il terzo numero indica lo step ['c', 'e', 'g', 'i']
+print(my_list[::3]) # Dall'inizio saltando 3 valori ['a', 'd', 'g', 'J']
