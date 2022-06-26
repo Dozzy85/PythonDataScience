8ede1762eff62fd34d854dc8024fc434505e6293
Andrea <82764646+Dozzy85@users.noreply.github.com>
2022-06-26 16:53:53 +0200

    PythonDataScience

---
+++ The While Loop.py
@@ -0,0 +1,5 @@
+counter = 0
+while counter < 12:
+    print(counter)
+    counter += 1
+print("Fine Ciclo While")
---
+++ the For Loop.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    print("Hello Python")
+
+for i in range(5):
+    print("Hello Python: ", i)
+    print("Second Line")
+
+mylist = [10, 100, 1000]
+for j in mylist:
+    print("j is equeal to: ", j)
