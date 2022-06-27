ad8574a01093908dfa15cdc6fd5cfe62c1cf3f48
Andrea <82764646+Dozzy85@users.noreply.github.com>
2022-06-27 23:17:21 +0200

    Homework

--- Python Fundamentals/Arrays in Python.py
+++ Python Fundamentals/Arrays in Python.py
@@ -13,3 +13,16 @@ l.pop()
 print(l)
 
 print(a.mean()) #calcola la media
+
+#Slicing di array
+print(a) #[ 2345    27   546   215 -1234]
+print(a[2:]) #[  546   215 -1234]
+print(b) #['12' '455' '63.3' 'True' 'abc']
+print(b[2:4]) #['63.3' 'True']
+print(b[0]) #12
+print(b[:]) #['12' '455' '63.3' 'True' 'abc']
+c = a.copy()
+print(c) #[ 2345    27   546   215 -1234]
+c[0] = 0
+print(a) #[ 2345    27   546   215 -1234]
+print(c) #[    0    27   546   215 -1234]
